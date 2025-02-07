f11ebf741e54f5a0d04cd44ecc2ae11d413ad109
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2025-02-06 21:00:25 -0700

    done with videos

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -2,6 +2,34 @@ function Header() {
   return (
     <header>
       <h1>Testing</h1>
+      <nav>
+        <ul>
+          <li>
+            {" "}
+            <a href="#" target="_blank">
+              Home
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="#" target="_blank">
+              About
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="#" target="_blank">
+              Services
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="#" target="_blank">
+              Contact
+            </a>{" "}
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
